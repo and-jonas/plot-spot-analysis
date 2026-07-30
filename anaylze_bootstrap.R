ac32d86f528ffa64ce7e1a63501d5e5b0cf5a892
Jonas Anderegg <jonas.anderegg@agroscope.admin.ch>
2026-07-30 08:34:25 +0200

    renamed files

--- anaylze_bootstrap.R
+++ anaylze_bootstrap.R
@@ -5,16 +5,31 @@ library(tidyverse)
 d <- readRDS("O:/Data-Work/22_Plant_Production-CH/224_Digitalisation/Jonas_Anderegg_Files/C_Manuscripts/2025_FocalStack_Downstream/data/bootstrap_results_placl.rds")
 
 n_eff_foc_placl <- lapply(d, "[[", 1) %>% unlist()
-n_eff_foc_placl_mean <- mean(n_eff_foc_placl)
+median(n_eff_foc_placl)
 quantile(
   n_eff_foc_placl,
   probs = c(0.025, 0.975)
 )
 
 n_eff_pos_placl <- lapply(d, "[[", 2) %>% unlist()
-n_eff_pos_placl_mean <- mean(n_eff_pos_placl)
+median(n_eff_pos_placl)
 quantile(
   n_eff_pos_placl,
   probs = c(0.025, 0.975)
 )
 
+d <- readRDS("O:/Data-Work/22_Plant_Production-CH/224_Digitalisation/Jonas_Anderegg_Files/C_Manuscripts/2025_FocalStack_Downstream/data/bootstrap_results_rust.rds")
+
+n_eff_foc_rust <- lapply(d, "[[", 1) %>% unlist()
+median(n_eff_foc_rust)
+quantile(
+  n_eff_foc_rust,
+  probs = c(0.025, 0.975)
+)
+
+n_eff_pos_rust <- lapply(d, "[[", 2) %>% unlist()
+median(n_eff_pos_rust)
+quantile(
+  n_eff_pos_rust,
+  probs = c(0.025, 0.975)
+)
